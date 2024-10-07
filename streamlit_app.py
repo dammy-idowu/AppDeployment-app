f3791d7ab00b66339c9955e6bd4cec17a40b42db
Damilola Idowu <167827855+dammy-idowu@users.noreply.github.com>
2024-10-07 05:21:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import scikit-learn as sklearn
+import sklearn
 from sklearn.ensemble import GradientBoostingClassifier
 
 # Inserting header for app
